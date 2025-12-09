99d64a19309c03caee82b21447a5b8210ca4c33e
Sofi <rozowasof@yandex.ru>
2025-12-09 16:32:34 +0300

    reformatted by pep8

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+
 def home(request):
     """Контроллер для домашней страницы"""
-    return render(request, 'catalog/home.html')
+    return render(request, "catalog/home.html")
+
 
 def contacts(request):
     """Контроллер для страницы контактов"""
-    return render(request, 'catalog/contacts.html')
+    return render(request, "catalog/contacts.html")
